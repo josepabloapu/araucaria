e6cd8393f2ee5caf82094f5b606ccf78750493d3
Marco Torres <marco.torres.810@gmail.com>
2015-05-25 09:59:36 -0600

    aux update

--- src/aux.v
+++ src/aux.v
@@ -3,6 +3,7 @@ Modules:
 		1)UPCOUNTER_POSEDGE
 		2)FFD_POSEDGE
 		3)FULL_ADDER
+		4)DUAL_READ_RAM
 
 //------------------------------------------------*/
 
@@ -68,3 +69,33 @@ endmodule
 
 //------------------------------------------------//
 
+module RAM_DUAL_READ_PORT # ( parameter DATA_WIDTH= 8, parameter ADDR_WIDTH=10, parameter MEM_SIZE=10 )
+(
+	input wire			Clock,
+	input wire			iWriteEnable,
+	input wire[ADDR_WIDTH-1:0]	iReadAddress0,
+	input wire[ADDR_WIDTH-1:0]	iReadAddress1,
+	input wire[ADDR_WIDTH-1:0]	iWriteAddress,
+	input wire[DATA_WIDTH-1:0]	iDataIn,
+	output reg [DATA_WIDTH-1:0] 	oDataOut0,
+	output reg [DATA_WIDTH-1:0] 	oDataOut1
+
+);
+
+reg [DATA_WIDTH-1:0] Ram [MEM_SIZE:0];		
+
+
+
+always @(posedge Clock) 
+begin 
+	
+		if (iWriteEnable) 
+			Ram[iWriteAddress] <= iDataIn; 
+			
+			oDataOut0 <=(( iWriteAddress == iReadAddress0 ) && iWriteEnable ) ? iDataIn : Ram[iReadAddress0 ] ;
+			oDataOut1 <= (( iWriteAddress == iReadAddress1 ) && iWriteEnable ) ? iDataIn : Ram[iReadAddress1 ] ;
+
+end 
+endmodule
+
+//------------------------------------------------//
---
+++ src/def.v
@@ -0,0 +1,14 @@
+//Instruction codification
+
+`define LDA   	8'h00
+`define LDB   	8'h01
+`define LDCA   	8'h02
+`define LDCB   	8'h03
+`define STA   	8'h04
+`define STA   	8'h05
+`define ADDA   	8'h06
+`define ADDB   	8'h07
+`define ADDCA  	8'h06
+`define ADDCB  	8'h07
+
+
