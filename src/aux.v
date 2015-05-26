9a4f0b6b195a6a3fed96da1b4ed1c70b9eb9567f
Marco Torres <marco.torres.810@gmail.com>
2015-05-26 11:16:45 -0600

    gantt diagram update

--- src/aux.v
+++ src/aux.v
@@ -3,7 +3,7 @@ Modules:
 		1)UPCOUNTER_POSEDGE
 		2)FFD_POSEDGE
 		3)FULL_ADDER
-		4)DUAL_READ_RAM
+		4)RAM
 
 //------------------------------------------------*/
 
@@ -69,22 +69,17 @@ endmodule
 
 //------------------------------------------------//
 
-module RAM_DUAL_READ_PORT # ( parameter DATA_WIDTH= 8, parameter ADDR_WIDTH=10, parameter MEM_SIZE=10 )
+module RAM_SINGLE_READ_PORT # ( parameter DATA_WIDTH= 8, parameter ADDR_WIDTH=10, parameter MEM_SIZE=10 )
 (
 	input wire			Clock,
 	input wire			iWriteEnable,
-	input wire[ADDR_WIDTH-1:0]	iReadAddress0,
-	input wire[ADDR_WIDTH-1:0]	iReadAddress1,
+	input wire[ADDR_WIDTH-1:0]	iReadAddress,
 	input wire[ADDR_WIDTH-1:0]	iWriteAddress,
 	input wire[DATA_WIDTH-1:0]	iDataIn,
-	output reg [DATA_WIDTH-1:0] 	oDataOut0,
-	output reg [DATA_WIDTH-1:0] 	oDataOut1
-
+	output reg [DATA_WIDTH-1:0] 	oDataOut
 );
 
 reg [DATA_WIDTH-1:0] Ram [MEM_SIZE:0];		
-
-
 
 always @(posedge Clock) 
 begin 
@@ -92,9 +87,7 @@ begin
 		if (iWriteEnable) 
 			Ram[iWriteAddress] <= iDataIn; 
 			
-			oDataOut0 <=(( iWriteAddress == iReadAddress0 ) && iWriteEnable ) ? iDataIn : Ram[iReadAddress0 ] ;
-			oDataOut1 <= (( iWriteAddress == iReadAddress1 ) && iWriteEnable ) ? iDataIn : Ram[iReadAddress1 ] ;
-
+		oDataOut <=(( iWriteAddress == iReadAddress ) && iWriteEnable ) ? iDataIn : Ram[iReadAddress ] ;
 end 
 endmodule
 
