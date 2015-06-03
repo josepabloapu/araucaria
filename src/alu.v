52c0b53aed3c5dd0bd08506cc825a4c01f9dcbcc
Marco Torres <marco.torres.810@gmail.com>
2015-06-03 08:27:11 -0600

    media update

--- src/alu.v
+++ src/alu.v
@@ -1,7 +1,6 @@
 `include "def.v"
 `include "aux.v"
 
-
 module alu
 (
 	//Entradas de control
@@ -14,8 +13,6 @@ module alu
 	input wire [9:0]	in2,
 
 	//Salidas
-	output wire [9:0] 	rReDir,
-	output wire [9:0] 	rWrDir,
 	output wire [7:0]  	rWrData
 );
 
@@ -26,54 +23,38 @@ begin
 	//Oparaciones de transferencia de datos
 	`LDCA:
 	begin
-		rReDir	<= 	10'b0;
-		rWrDir	<= 	10'b0;
 		rWrData	<= 	8'b0;			
 	end
 	`LDCB:
 	begin
-		rReDir	<= 	10'b0;
-		rWrDir	<= 	10'b0;
 		rWrData	<= 	8'b0;			
 	end
 
 	`LDA:
 	begin
-		rReDir	<= 	in1[7:0];
-		rWrDir	<= 	10'b0;
 		rWrData	<= 	8'b0;			
 	end
 	`LDB:
 	begin
-		rReDir	<= 	in1[7:0];
-		rWrDir	<= 	10'b0;
 		rWrData	<= 	8'b0;			
 	end
 	`STA:
 	begin
-		rReDir	<= 	10'b0;
-		rWrDir	<= 	in1;
-		rWrData	<= 	in2[7:0];		
+		rWrData	<= 	in1[7:0];		
 	end
 	`STB:
 	begin
-		rReDir	<= 	10'b0;
-		rWrDir	<= 	in2;
-		rWrData	<= 	in1[7:0];		
+		rWrData	<= 	in2[7:0];		
 	end
 	//-------------------------------------
 
 	//Oparaciones aritmeticas
 	`ADDA:
 	begin
-		rReDir	<= 	10'b0;
-		rWrDir	<= 	10'b0;
 		rWrData	<= 	in1[7:0] + in2[7:0];	
 	end
 	`ADDB:
 	begin
-		rReDir	<= 	10'b0;
-		rWrDir	<= 	10'b0;
 		rWrData	<= 	in1[7:0] + in2[7:0];
 	end
 	//-------------------------------------
@@ -89,17 +70,12 @@ begin
 	//Otras
 	`NOP:
 	begin
-		rReDir	<= 	1'b0;
-		rWrDir	<= 	1'b0;
-		rWrData	<= 	1'b0;			
+		rWrData	<= 	8'b0;			
 	end
 	//-------------------------------------
-	
 	default:
 	begin
-		rReDir	<= 	1'b0;
-		rWrDir	<= 	1'b0;
-		rWrData	<= 	1'b0;	
+		rWrData	<= 	8'b0;	
 	end	
 	endcase	
 end
