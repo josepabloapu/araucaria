18076aebaeb0bfdb7ee1d93aadee03e811dec4fa
Jose Pablo Apú Picado <japup@nabuco>
2015-07-01 12:14:09 -0600

    arreglos del decoder

--- src/waka.v
+++ src/waka.v
@@ -63,10 +63,11 @@ decodec d0
 
 initial 
 begin
-	Clock = 0;
-	#05 	Reset=1; #05 Reset=0;
+	#00 Clock = 0;
+	#05 Reset = 1; 
+	#05 Reset = 0;
 
-	#5	data = 16'b000000010000000;	flaga = 3'b000;		flagb = 3'b000;
+	#10	data = 16'b000000010000000;	flaga = 3'b000;		flagb = 3'b000;
 	#10 data = 16'b000001010000000;	flaga = 3'b000;		flagb = 3'b000;
 	#10 data = 16'b000010010000000; flaga = 3'b000;		flagb = 3'b000;	
 	#10	data = 16'b000011010000000; flaga = 3'b000;		flagb = 3'b000;
@@ -74,18 +75,18 @@ begin
 	#10 data = 16'b000101010000000;	flaga = 3'b000;		flagb = 3'b000;
 	#10 data = 16'b000110010000000; flaga = 3'b000;		flagb = 3'b000;	
 	#10	data = 16'b000111010000000; flaga = 3'b000;		flagb = 3'b000;
-	#10	data = 16'b000000010000000;	flaga = 3'b000;		flagb = 3'b000;
-	#10 data = 16'b000001010000000;	flaga = 3'b000;		flagb = 3'b000;
-	#10 data = 16'b000010010000000; flaga = 3'b000;		flagb = 3'b000;	
-	#10	data = 16'b000011010000000; flaga = 3'b000;		flagb = 3'b000;
-	#10	data = 16'b000100010000000;	flaga = 3'b000;		flagb = 3'b000;
-	#10 data = 16'b000101010000000;	flaga = 3'b000;		flagb = 3'b000;
-	#10 data = 16'b000110010000000;	flaga = 3'b000;		flagb = 3'b000;	
-	#10	data = 16'b000111010000000;	flaga = 3'b000;		flagb = 3'b000;
-	#10 data = 16'b001000010000000;	flaga = 3'b000;		flagb = 3'b000;	
-	#10	data = 16'b001001010000000;	flaga = 3'b000;		flagb = 3'b000;
-	#10 data = 16'b001010010000000;	flaga = 3'b000;		flagb = 3'b000;	
-	#10	data = 16'b001011010000000;	flaga = 3'b000;		flagb = 3'b000;
+	#10	data = 16'b001000010000000;	flaga = 3'b000;		flagb = 3'b000;
+	#10 data = 16'b001001010000000;	flaga = 3'b000;		flagb = 3'b000;
+	#10 data = 16'b001010010000000; flaga = 3'b000;		flagb = 3'b000;	
+	#10	data = 16'b001011010000000; flaga = 3'b000;		flagb = 3'b000;
+	#10	data = 16'b001100010000000;	flaga = 3'b000;		flagb = 3'b000;
+	#10 data = 16'b001101010000000;	flaga = 3'b000;		flagb = 3'b000;
+	#10 data = 16'b001110010000000;	flaga = 3'b000;		flagb = 3'b000;	
+	#10	data = 16'b001111010000000;	flaga = 3'b000;		flagb = 3'b000;
+	#10 data = 16'b010000010000000;	flaga = 3'b000;		flagb = 3'b000;	
+	#10	data = 16'b010001010000000;	flaga = 3'b000;		flagb = 3'b000;
+	#10 data = 16'b010010010000000;	flaga = 3'b000;		flagb = 3'b000;	
+	#10	data = 16'b010011010000000;	flaga = 3'b000;		flagb = 3'b000;
 
 	#10
 	$finish;
